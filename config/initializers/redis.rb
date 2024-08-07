8ef8d26a29ff5a79d2f39d143299b60b17a2eb7f
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-07 23:49:06 +0530

    minor

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 $redis = Redis.new(
-    host: ENV.fetch('redis_host') { 'localhost' },
-    port: ENV.fetch('redis_port', 6379),
-    password: ENV.fetch('redis_password') { Rails.application.credentials.redis[:password] },
-    db: 0
+    host: Rails.application.credentials.redis[:host],
+    port: Rails.application.credentials.redis[:port],
+    password: Rails.application.credentials.redis[:password],
+    db: Rails.application.credentials.redis[:db]
   )
