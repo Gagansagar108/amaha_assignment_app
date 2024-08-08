6550cc1111319a31e196e2694efeb512efec5dbb
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-08 23:17:15 +0530

    minor

--- app/constants/distance_constants.rb
+++ app/constants/distance_constants.rb
@@ -2,4 +2,5 @@ class DistanceConstants
     EARTH_RADIUS = 6335.439 #km
     OFFICE_LAT = 19.0590317
     OFFICE_LONG = 72.7553452
+    DISTANCE_RANGE = 100 #km
 end 
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,9 @@ class CustomersController < ApplicationController
     def get_nearest_customers
         params = get_params
         
-        file_url = params[:file_url]
+        file_data = get_file_data(params)
         
-        file_data = get_file_data(file_url)
-        
-        result = filter_data(file_data)
+        result = filter_data(file_data, params)
         
         sort_result(result)
         
@@ -21,20 +19,23 @@ class CustomersController < ApplicationController
         params.as_json.to_h.deep_symbolize_keys
     end
 
-    def get_file_data(file_url)
-        FileReader.get_text_file_data(file_url)
+    def get_file_data(params)
+        FileReader.get_text_file_data(params[:file_url))
     end 
 
-    def filter_data(file_data)
+    def filter_data(file_data,params)
         result = []
-        lat_y = DistanceConstants::OFFICE_LAT
-        long_y = DistanceConstants::OFFICE_LONG
+       
+        lat_y = params[:office_lat] || DistanceConstants::OFFICE_LAT
+        long_y = params[:office_long] || DistanceConstants::OFFICE_LONG
+       
         file_data.each do |data|
             lat_x = data["latitude"].to_f
             long_x = data["longitude"].to_f
             distance = DistanceCalculator.get_Haversine_distance(lat_x, long_x, lat_y, long_y)
-            result.push(data.slice('user_id', 'name')) 
+            result.push(data.slice('user_id', 'name')) if distance <= params[:distance_range] || DistanceConstants::DISTANCE_RANGE
         end
+       
         result
     end 
 
