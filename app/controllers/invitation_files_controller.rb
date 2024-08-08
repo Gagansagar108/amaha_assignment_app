da71fc9a5c06c55b76bb021ce3557f769ce32a8d
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-08 13:15:17 +0530

    minor

--- app/controllers/invitation_files_controller.rb
+++ app/controllers/invitation_files_controller.rb
@@ -2,7 +2,13 @@ class InvitationFilesController < ApplicationController
     skip_before_action :verify_authenticity_token, only: :create
     def create
         binding.pry
-        blob = ActiveStorage::Blob.create_before_direct_upload!(blob_params)
+        blob = ActiveStorage::Blob.create_before_direct_upload!(
+            filename: blob_params[:filename],
+            byte_size: blob_params[:byte_size],
+            checksum: blob_params[:checksum],
+            content_type: blob_params[:content_type],
+            metadata: blob_params[:metadata]
+          )
         render json: direct_upload_json(blob)
     end
     
