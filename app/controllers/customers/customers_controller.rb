2ee28b4ed9876d0a1f588ed031884a6cce0fa276
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-09 18:33:40 +0530

    minor

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -31,9 +31,9 @@ module Customers
             long_y = (params[:office_long] || DistanceConstants::OFFICE_LONG).to_f
         
             file_data.each do |data|
-                binding.pry
-                lat_x = data["latitude"].to_f
-                long_x = data["longitude"].to_f
+                data.to_h.deep_symbolize_keys!
+                lat_x = data[:latitude].to_f
+                long_x = data[:longitude].to_f
                 distance = DistanceCalculator.get_Haversine_distance(lat_x, long_x, lat_y, long_y)
                 result.push(data.slice('user_id', 'name').merge!(distance: round(distance,2))) if distance <= (params[:distance_range] || DistanceConstants::DISTANCE_RANGE).to_f
             end
@@ -41,7 +41,7 @@ module Customers
         end 
 
         def sort_result(result)
-            result.sort_by!{|res| res["user_id"]}
+            result.sort_by!{|res| res[:user_id]}
         end 
     end 
 end 
