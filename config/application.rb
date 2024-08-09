c66e61c504ac0b636ad1ce8185b2f0e748d7347c
Ubuntu <ubuntu@ip-172-31-44-118.ap-south-1.compute.internal>
2024-08-09 04:54:16 +0000

    minor

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'rack/attack'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -19,7 +21,9 @@ module AmahaAssignmentApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
-
+    
+    config.middleware.use Rack::Attack
+    
     config.eager_load_paths += %W[#{config.root}/lib/utils]
   end
 end
