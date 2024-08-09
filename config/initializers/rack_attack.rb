780b5b28d085a5aaa2f72713b51ec1d9c2293ec6
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-09 10:23:50 +0530

    rack

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,11 @@
+module Rack
+    class Attack  
+      throttle('upload_invitation_file', limit: 3, period: 5.minutes) do |request| 
+        request.params.deep_symbolize_keys[:request_ip] if request.path.ends_with?('get_freight_trend') 
+      end
+
+      throttle('get_nearest_customers', limit: 3, period: 5.minutes) do |request|
+        request.params.deep_symbolize_keys[:request_ip] if request.path.ends_with?('get_freight_trend')
+      end
+    end   
+end 
