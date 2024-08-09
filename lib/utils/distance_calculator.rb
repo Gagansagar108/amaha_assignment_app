ec0762bba6113c81e82cb30ed23201190cf398d5
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-09 18:08:34 +0530

    minor

--- lib/utils/distance_calculator.rb
+++ lib/utils/distance_calculator.rb
@@ -1,5 +1,6 @@
 module DistanceCalculator
     def self.get_Haversine_distance(lat_x, long_x, lat_y, long_y)
+        binding.pry
         unit_rad = Math::PI/180
         del_phi = (lat_y - lat_x)*unit_rad
         del_lambda = (long_y - long_x)*unit_rad
