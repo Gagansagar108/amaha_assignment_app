12bb6894588d4ba0dc873dba941e274954288fba
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-08-09 18:51:17 +0530

    minor

--- spec/controllers/customers/customers_controller_spec.rb
+++ spec/controllers/customers/customers_controller_spec.rb
@@ -53,6 +53,7 @@ RSpec.describe Customers::CustomersController, type: :controller do
         data = json_response['data']
         expect(data).to be_an(Array)
         expect(data).to be_empty
+        binding.pry
         expect(data.count).to be_an(0)
       end
     end
