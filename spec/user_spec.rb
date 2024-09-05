cf24e93c5b9684a1cc42cb31f98f5dab6d434a84
gagan <gaganiitb108@gmail.com>
2024-09-05 10:37:30 +0530

    yes

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   describe '#bark' do
     it 'returns the "user speak"'do
       user = User.new
-      expect(user.bark).to eql?('user speak')
+      expect(user.bark).to eql('user speak')
     end
   end
 end
