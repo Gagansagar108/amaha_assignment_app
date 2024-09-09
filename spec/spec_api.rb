de226a77f99a6316cda74e8144173e6ab4c3050b
gagan <gaganiitb108@gmail.com>
2024-09-09 13:55:01 +0530

    auto

--- spec/spec_api.rb
+++ spec/spec_api.rb
@@ -1,8 +1,9 @@
  require 'rails_helper'
   #https://amahastorage.s3.ap-south-1.amazonaws.com/tq0jdn2wj30j8336p89gj7epltwa
- Rspec.describe '' do
+ Rspec.describe 'request' do
 
   it do
-    get('/')
+    get('/customers/get_nearest_customers')
+    binding.pry
   end
  end
