4f5790f4621ce7814f15ac35ab8a17d3cd07643a
gagan <gaganiitb108@gmail.com>
2024-09-09 14:17:52 +0530

    auto

--- spec/spec_api.rb
+++ spec/spec_api.rb
@@ -3,7 +3,7 @@
  RSpec.describe 'request', type: :request do
   describe 'GET /customers/get_nearest_customers' do
     it 'fetch data' do
-      get('/customers/get_nearest_customers')
+      get('/customers/get_nearest_customers', params: {file_url: 'https://amahastorage.s3.ap-south-1.amazonaws.com/tq0jdn2wj30j8336p89gj7epltwa' })
       binding.pry
     end
   end
