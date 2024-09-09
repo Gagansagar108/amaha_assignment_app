20df33d0fed54b5826ced7e4612fb4426cf9de87
gagan <gaganiitb108@gmail.com>
2024-09-09 13:59:20 +0530

    auto

--- spec/spec_api.rb
+++ spec/spec_api.rb
@@ -2,8 +2,9 @@
   #https://amahastorage.s3.ap-south-1.amazonaws.com/tq0jdn2wj30j8336p89gj7epltwa
  RSpec.describe 'request' do
 
-  it do
-    get('/customers/get_nearest_customers')
+  it 'do something' do
+    describe 'GET customers/get_nearest_customers' do
     binding.pry
+    end
   end
  end
