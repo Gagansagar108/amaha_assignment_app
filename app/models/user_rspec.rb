8f08b552b49476b6793ad6c93c1a019943ae1031
gagan <gaganiitb108@gmail.com>
2024-09-09 14:50:01 +0530

    auto

---
+++ app/models/user_rspec.rb
@@ -0,0 +1,3 @@
+class UserRspec < ActiveRecord
+
+end
--- db/migrate/20240909090508_users.rb
+++ db/migrate/20240909090508_users.rb
@@ -1,6 +1,6 @@
 class Users < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |col|
+    create_table :user_rspecs do |col|
       col.string :name
       col.string :email
       col.timestamps
