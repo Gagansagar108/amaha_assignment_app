ddbf9a05504fd82044e975d757413422cf67c6d1
gagan <gaganiitb108@gmail.com>
2024-09-09 14:07:34 +0530

    auto

--- spec/spec_api.rb
+++ spec/spec_api.rb
@@ -3,5 +3,10 @@
 #customers/get_nearest_customers
  RSpec.describe 'request' do
   describe 'GET /customers/get_nearest_customers' do
+    it 'fetch data' do
+      get('/customers/get_nearest_customers')
+
+      binding.pry
+    end
   end
  end
